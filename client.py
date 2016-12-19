f2920d278577aa2f895fa9f952e2085bf047aeb2
JerryWang304 <wang_feng_bro@163.com>
2016-12-19 09:32:06 +0800

    not very stable

--- client.py
+++ client.py
@@ -1,13 +1,113 @@
 import socket
+import rsa
+import random
+from Crypto.Cipher import AES
+from numpy.random import permutation
+from binascii import b2a_hex, a2b_hex
+mode = AES.MODE_CBC
 HOST = '127.0.0.1'
 PORT = 8001
 
+ 
+def double_decode(k1,k2,ciphertext):
+     decryptor = AES.new(k1,mode,b'0000000000000000')
+     plain_text = decryptor.decrypt(ciphertext)
+     decryptor = AES.new(k2,mode,b'0000000000000000')
+     plain_text = decryptor.decrypt(plain_text)
+     return plain_text
+
 s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
 s.connect((HOST, PORT))
-msg = s.recv(1024) 
-print msg
+num_bits = int(s.recv(1))
+print "At most %d bits" % num_bits
+bob_wealth = raw_input("Input Bob's wealth: ")
+print "Bob's wealth = ",bob_wealth
 while True:
-    num = raw_input("Please input one bit:")
-    s.send(num)  
-    data = s.recv(1024)
-    print data
+    # receive i
+    i = s.recv(1)
+    print i
+    i = int(i)
+    print "##### %d-th comparision #####" % (i+1)
+    print "Receiving garble circuit (permutation)"
+    c1 = s.recv(32)
+    print "c1 = ",c1
+    c2 = s.recv(32)
+    print "c2 = ",c2
+    c3 = s.recv(32)
+    print "c3 = ",c3
+    c4 = s.recv(32)
+    print "c4 = ",c4
+    print "Receiving one key representing Alice's input: "
+    alice_key = s.recv(16)
+    print alice_key
+    print "Run OT to get next key"
+    print "generating public and private keys..."
+    (public_key,private_key) = rsa.newkeys(1024)
+    
+    # for 0, Bob should get the key representing 1
+    if bob_wealth[i] == '1':
+        print "sending public key 1"
+        print "n = ",public_key['n']
+        #print "e = ",public_key1['e']
+        s.send(str(public_key['n']))
+        #s.send(str(public_key1['e']))
+        print "\n"
+        print "sending public key 2"
+        n = random.randint(10**90,10**100)
+        print "n = ", n
+        #print "e = ",public_key2['e']
+        s.send(str(n))
+        #s.send(str(public_key2['e']))
+        print "\n"
+    # for 1, Bob should get the key representing  0
+    else:
+        print "sending public key 2"
+        n = random.randint(10**90,10**100)
+        print "n = ",n
+        #print "e = ",public_key2['e']
+        s.send(str(n))
+        #s.send(str(public_key2['e']))
+        print "\n"
+        print "sending public key 1"
+        print "n = ",public_key['n']
+        #print "e = ",public_key1['e']
+        s.send(str(public_key['n']))
+        #s.send(str(public_key1['e']))
+        print "\n"                
+    print "Receiving two encryped keys..."
+    x1 = s.recv(1024)
+    print "x1 = ",(x1)
+
+    x2 = s.recv(1024)
+    print "x2 = ",(x2)
+    # encode to get key representing bob's 0 or 1
+    if bob_wealth[i] == '0':
+        bob_key = rsa.decrypt(x2,private_key)
+    elif bob_wealth[i] == '1':
+        bob_key = rsa.decrypt(x1,private_key)
+    print "bob_key = ", bob_key
+
+    # Now Bob tries to decode c1, c2, c3, c4 respectively 
+    # using two keys achieved
+    p1 = double_decode(alice_key,bob_key,a2b_hex(c1))
+    print "p1 = ",p1
+    p2 = double_decode(alice_key,bob_key,a2b_hex(c2))
+    print "p2 = ",p2
+    p3 = double_decode(alice_key,bob_key,a2b_hex(c3))
+    print "p3 = ",p3
+    p4 = double_decode(alice_key,bob_key,a2b_hex(c4))
+    print "p4 = ",p4
+    # send the decoded keys to Alice
+    s.send(p1)
+    s.send(p2)
+    s.send(p3)
+    s.send(p4)
+    result = s.recv(1)
+    
+    if result == '0':
+        break
+    
+    if (i+1) >= num_bits:
+        break
+s.close()
+print "result = ",result
--- server.py
+++ server.py
@@ -1,4 +1,10 @@
 import socket
+import random
+import rsa
+from Crypto.Cipher import AES
+from numpy.random import permutation
+from binascii import b2a_hex, a2b_hex
+mode = AES.MODE_CBC
 
 HOST = '127.0.0.1'
 PORT = 8001
@@ -9,16 +15,135 @@ s.bind((HOST, PORT))
 s.listen(1)
 
 print 'Alice at: %s:%s' %(HOST, PORT)
-print 'waiting for Bob...'
-n = input("How many bits: ")
 
+n = input("How many bits: ")
+print 'waiting for Bob...'
 
 conn, addr = s.accept()
 print 'Connected by Bob', addr
-conn.send("At most %d bits" % n)
+conn.send(str(n))
+
+def double_encode(k1,k2,plaintext):
+    encryptor = AES.new(k1,mode,b'0000000000000000')
+    ciphertext1 = encryptor.encrypt(plaintext)
+    # cipher1 = b2a_hex(ciphertext1)
+    encryptor = AES.new(k2,mode,b'0000000000000000')
+    ciphertext2 = encryptor.encrypt(ciphertext1)
+    return ciphertext2
+
+
+alice_wealth = raw_input("Input Alice's wealth in binary format: ")
+print "alice wealth = ",alice_wealth
+i = 0
+result = 1
 while True:
-    data = int(conn.recv(1024))
-    print data
+
+    print "##### %d-th comparision #####" % (i+1)
+    conn.send(str(i))
+    # generate 6 random numbers
+    k = []
+    chars = [str(j) for j in range(0,10)]
+    chars.extend([chr(j) for j in range(97,123)])
+    for j in range(6):
+        temp_k = ''
+        for j in range(16):
+            temp_k += random.choice(chars)
+        k.append(temp_k)
+
+    # k[0]: 0
+    # k[1]: 1
+    # k[2]: 0
+    # k[3]: 1
+    # k[4]: 0
+    # k[5]: 1
+    print "all the keys: "
+    print k
+    # four encoded numbers
+    # round 1
+    c1 = double_encode(k[2],k[0],k[4])
+    # round 2
+    c2 = double_encode(k[3],k[0],k[5])
+    # round 3
+    c3 = double_encode(k[2],k[1],k[5])
+    # round 4
+    c4 = double_encode(k[3],k[1],k[5])
+    # garble_table = [c1,c2,c3,c4]
+    # garble_table = permutation(garble_table)
+    # send garble circuit to Bob
+    print "Sending garble circuit"
+    print "c1 = ",b2a_hex(c1)
+    print "c2 = ",b2a_hex(c2)
+    print "c3 = ",b2a_hex(c3)
+    print "c4 = ",b2a_hex(c4)
+    print "sending c2"
+    conn.send(b2a_hex(c2))
+    print "sending c1"
+    conn.send(b2a_hex(c1))
+    print "sending c3"
+    conn.send(b2a_hex(c3))
+    print "sending c4"
+    conn.send(b2a_hex(c4))
+    # send one key that represents 0 or 1
+    print "sending one key: ",
+    if alice_wealth[i] == '0':
+        conn.send(k[0])
+        print k[0],
+        print ":  0"
+    elif alice_wealth[i] == '1':
+        conn.send(k[1])
+        print k[1],
+        print ":  1"
+    # send k[2] or k[3]
+    print "Run OT to sent next key"
+
+    print "Receiving public key 1"
+    n1 = int(conn.recv(1024))
+    print "n1 = ",n1
+    #e1 = int(conn.recv(5))
+    #print "e1 = ",e1
+    public_key1 = rsa.PublicKey(n1,65537)
     
-    conn.send("Alice received you message.") 
-conn.close() 
+    
+    print "Receiving public key 2"
+    n2 = int(conn.recv(1024))
+    print "n2 = ",n2
+    #e2 = int(conn.recv(5))
+    #print "e2 = ",e2
+    public_key2 = rsa.PublicKey(n2,65537)
+    
+    
+
+    # encode k[2] and k[3]
+    print "k[2] = ",k[2]
+    print "k[3] = ",k[3]
+    cipher1 = rsa.encrypt(k[2],public_key1)
+    cipher2 = rsa.encrypt(k[3],public_key2)
+    print "sending encrypted keys..."
+    print "cipher1 = ",cipher1
+    
+    conn.send(cipher1)
+    print "cipher2 = ",cipher2
+    conn.send(cipher2)
+    print "Receiving Bob's decrypted keys"
+    p1 = conn.recv(16)
+    p2 = conn.recv(16)
+    p3 = conn.recv(16)
+    p4 = conn.recv(16)
+    p = [p1,p2,p3,p4]
+    print "p = ",p
+    # if we get p4, it means that f(a,b) = 0. Thus, a<b
+    # and we should stop now 
+    for pi in p:
+        if pi in k and pi == k[4]:
+
+            result = 0
+    # send result to Bob
+    conn.send(str(result))
+    if result == 0:
+        break
+    if (i+1) >= n:
+        break
+    i = i+1
+
+conn.close()
+print "result = ",result
